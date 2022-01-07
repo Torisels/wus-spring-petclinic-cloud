f47cb13f2e54268dd0ab1538d9d7dc609707bd08
torisels <01149695@pw.edu.pl>
2022-01-07 13:04:02 +0100

    Add creation scripts

---
+++ scripts/create_cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
+# enable needed APIs
+gcloud services enable \
+cloudapis.googleapis.com \
+container.googleapis.com \
+--project=$PROJECT_ID
+
+gcloud container clusters create ${CLUSTER_NAME} \
+--project=$PROJECT_ID \
+--enable-ip-alias \
+--scopes=cloud-platform \
+--num-nodes=3 \
+--machine-type=e2-small \
+--disk-size=20GB \
+--logging=SYSTEM,WORKLOAD \
+--monitoring=SYSTEM,WORKLOAD
